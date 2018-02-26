aa243ffbc9da5add9e12b340c6cffd6dc37f8473
alvi0115 <aleksandr.vilchinskii@netcracker.com>
2018-02-27 01:40:15 +0300

    Test git plugin in Idea

--- src/main/java/com/eight/zero/three/Algorithm.java
+++ src/main/java/com/eight/zero/three/Algorithm.java
@@ -3,6 +3,9 @@ package com.eight.zero.three;
 import com.eight.zero.three.input.Input;
 import com.eight.zero.three.output.Output;
 
+/**
+ * I am just testing git plugin in Idea.
+ */
 public class Algorithm {
     public static Output run(final Input input) {
         return new Output(
