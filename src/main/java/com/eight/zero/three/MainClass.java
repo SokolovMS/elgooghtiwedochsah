88f13bab35a56d026ed2f364a6702bfd401cf921
alvi0115 <aleksandr.vilchinskii@netcracker.com>
2018-02-27 01:31:43 +0300

    Use local directory path for an input file

--- src/main/java/com/eight/zero/three/MainClass.java
+++ src/main/java/com/eight/zero/three/MainClass.java
@@ -2,6 +2,6 @@ package com.eight.zero.three;
 
 public class MainClass {
     public static void main(String[] args) {
-        MainHandler.run("E:/Development/maven/elgooghtiwedochsah/src/main/resources/example.in");
+        MainHandler.run("src/main/resources/example.in");
     }
 }
