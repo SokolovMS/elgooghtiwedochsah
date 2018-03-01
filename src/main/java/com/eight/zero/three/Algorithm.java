ca85a9dbe0ee23d67034cfb00e16c7a16b571347
Mikhail <m.s.sokolov.92@gmail.com>
2018-03-01 19:17:20 +0300

    Mandatory Algorithm fix

--- src/main/java/com/eight/zero/three/Algorithm.java
+++ src/main/java/com/eight/zero/three/Algorithm.java
@@ -10,9 +10,9 @@ public class Algorithm {
     public static Output run(final Input input) {
         return new Output(
                 3,
-                new Slice(0, 0, 2, 1),
+                new Slice(0, 2, 0, 1),
                 new Slice(0, 2, 2, 2),
-                new Slice(0, 3, 2, 4)
+                new Slice(0, 2, 3, 4)
         );
     }
 }
