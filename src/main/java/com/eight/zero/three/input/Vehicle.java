678a639f5393e92ef1e4253ba894e6f036f8f12f
alvi0115 <aleksandr.vilchinskii@netcracker.com>
2018-03-01 22:27:50 +0300

    Check if car is busy

--- src/main/java/com/eight/zero/three/input/Vehicle.java
+++ src/main/java/com/eight/zero/three/input/Vehicle.java
@@ -1,5 +1,7 @@
 package com.eight.zero.three.input;
 
+import static com.eight.zero.three.algorithm.Utils.getDistance;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +14,27 @@ public class Vehicle {
         this.rides = new ArrayList<>();
     }
 
-    void assignRide(final Ride ride) {
+    public void assignRide(final Ride ride) {
         rides.add(ride);
+    }
+
+    public Integer getNumber() {
+        return number;
     }
 
     public List<Ride> getRides() {
         return rides;
+    }
+
+    public boolean isBusy(final int curStep) {
+        int busySteps = 0;
+        Coord prevFinish = new Coord(0,0);
+
+        for (Ride ride : rides) {
+            busySteps += getDistance(prevFinish, ride.getSrc());
+            prevFinish = ride.getDst();
+        }
+
+        return busySteps > curStep;
     }
 }
