7a0a29a84ebade2234a747a4af64762173b3fbb3
Egor Sinenkov <e.sinenkov@gmail.com>
2018-03-01 22:34:58 +0300

    Sort has been added

--- src/main/java/com/eight/zero/three/algorithm/Utils.java
+++ src/main/java/com/eight/zero/three/algorithm/Utils.java
@@ -1,9 +1,30 @@
 package com.eight.zero.three.algorithm;
 
 import com.eight.zero.three.input.Coord;
+import com.eight.zero.three.input.Ride;
+
+import java.util.Collections;
+import java.util.List;
 
 public class Utils {
     public static int getDistance(final Coord a, final Coord b) {
         return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY());
+    }
+
+    public static List<Ride> sortRides(List<Ride> rides) {
+        // Sort by distance
+        Collections.sort(rides, (ride1, ride2) -> {
+            return ride1.getScore() - ride2.getScore();
+        });
+
+        // Sort by start time
+        Collections.sort(rides, (ride1, ride2) -> {
+            int ride1StartTime = ride1.getStartInterval().gettMin();
+            int ride2StartTime = ride2.getStartInterval().gettMin();
+
+            return ride1StartTime - ride2StartTime;
+        });
+
+        return rides;
     }
 }
